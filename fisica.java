0b2d68eb2420829c41030e33cc1547ab45d2a504
JIsraelMQ <160273380+JIsraelMQ@users.noreply.github.com>
2024-02-16 12:49:38 -0600

    Add files via upload
    Programa hecho en clase con bastantes detalles

---
+++ fisica.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el vector:");
+        int tamaño = scanner.nextInt();
+
+        System.out.println("Ingrese los elementos del vector:");
+
+        for (int i = 0; i < tamaño; i++) {
+            System.out.print("Elemento " + (i + 1) ");
+            vector[u] = scanner.nextInt();
+        }
+
+        System.out.println("El vector ingresado es:");
+        for (int i = 0; i < tamaño; i++) {
+            System.out.print(vector[u] + " ");
+        }
